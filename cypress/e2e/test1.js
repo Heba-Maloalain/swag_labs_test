91dc862a95a4cd55cc7b0778f44db5aa2ad87897
Heba-Maloalain <maloalainheba@gmail.com>
2023-06-25 17:15:00 +0300

    update

---
+++ cypress/e2e/test1.js
@@ -0,0 +1,28 @@
+beforeEach(() => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+    cy.clearAllSessionStorage();
+    cy.log('Cache and cookies cleared');
+    cy.reload()
+});
+///<reference types = "Cypress"/> 
+describe('log in', () => {
+
+
+    it('add all item to card', () => {
+        cy.visit("https://www.saucedemo.com/")
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+        let expecteditems = 6;
+        for (let i = 0; i < expecteditems ; i++) {
+            if (i % 2 == 0) {
+                let Array = cy.get('div#inventory_container').find('.btn').eq(i).click()
+
+            }
+        }
+
+
+
+    })
+});
